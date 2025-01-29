28d7237d717b311d59d25417a43204d7ca4e28ec
erdemakinlux <erdem.akin.lux>
2025-01-30 00:04:01 +0100

    new funct

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    words = file_contents.split()
-    print(len(words))
+    book = file_contents.lower()
+    myDict = {}
+    for char in book:
+        if len(myDict) == 0:
+            myDict[char] = 1
+        else
+            if char in myDict:
+                myDict[char] += 1
+            else
+                myDict[char] = 1 
+    print(myDict)
+
+    
     
 if __name__ == "__main__":
     main()
